ad16a16ca2ba1c9001e13bb47877f3ede7a50db7
foxbao <jidushanbojue1776@hotmail.com>
2022-03-01 21:26:23 +0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,106 @@
+
+#include <vector>
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <sys/time.h>
+
+#include "dbscan.hpp" //<-- include dbscan library
+
+using namespace std;
+std::vector<std::string> splitWithStl(const std::string &str, const std::string &pattern)
+{
+    std::vector<std::string> resVec;
+
+    if ("" == str)
+    {
+        return resVec;
+    }
+    //方便截取最后一段数据
+    std::string strs = str + pattern;
+
+    size_t pos = strs.find(pattern);
+    size_t size = strs.size();
+
+    while (pos != std::string::npos)
+    {
+        std::string x = strs.substr(0, pos);
+        resVec.push_back(x);
+        strs = strs.substr(pos + 1, size);
+        pos = strs.find(pattern);
+    }
+
+    return resVec;
+}
+
+float stringToFloat(const string &i)
+{
+    using namespace std;
+    stringstream sf;
+    float score = 0;
+    sf << i;
+    sf >> score;
+    return score;
+}
+
+std::vector<std::pair<float, float>> openFile(char *filename)
+{
+    // std::vector<vec3f> dataset;
+    using namespace std;
+    fstream file;
+    file.open(filename, ios::in);
+    if (!file)
+    {
+        cout << "Open File Failed!" << endl;
+    }
+
+    int i = 1;
+    int idx_pt = 0;
+    std::vector<std::pair<float, float>> dataset;
+    while (!file.eof())
+    {
+        string temp;
+        file >> temp;
+        std::vector<std::string> resVec = splitWithStl(temp, ",");
+        stringToFloat(resVec[0]);
+        std::pair<float, float> data;
+        data.first = stringToFloat(resVec[0]);
+        data.second = stringToFloat(resVec[1]);
+        dataset.push_back(data);
+    }
+    file.close();
+    cout << "successful!" << endl;
+    return dataset;
+}
+
+int main(int argc, char *argv[])
+{
+
+    int minPoints = 4;
+    float epsilon = 40;
+    std::vector<std::pair<float, float>> data;
+    std::pair<float, float> data1;
+
+    data = openFile("feature.txt");
+    // data1.first=100;
+    // data1.second=100;
+    // data.push_back(data1);
+    // std::pair<float,float> data2;
+    // data1.first=200;
+    // data1.second=100;
+    // data.push_back(data2);
+    struct timeval t1, t2;
+    for (int i = 0; i < 10; i++)
+    {
+        gettimeofday(&t1, NULL);
+        dbscan(data, epsilon, minPoints);
+        gettimeofday(&t2, NULL);
+        double deltaT = (t2.tv_sec - t1.tv_sec) * 1000000 + t2.tv_usec - t1.tv_usec;
+        std::cout << "time_comsumed:" << deltaT / 1000000 << std::endl;
+    }
+
+    // std::vector<std::vector<unsigned long>> iClusters = dbscan::cluster(points, epsilon, minPoints);
+
+    return 0;
+}
